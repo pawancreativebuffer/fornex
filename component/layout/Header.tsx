94febc7105f9de4a27b41b768568d4965dd4f479
pawancreativebuffer <pawansinghania@creativebuffer.com>
2026-03-17 19:04:47 +0530

    test

--- component/layout/Header.tsx
+++ component/layout/Header.tsx
@@ -21,10 +21,10 @@ export default function Header() {
     ];
 
     return (
-        <header className="w-full py-10 px-4 relative z-50">
-            <div className="max-w-[1400px] mx-auto px-6 py-3 flex items-center justify-between gap-4 
-            bg-white/100 backdrop-blur-lg border border-white/60 rounded-full 
-            shadow-[0_8px_32px_rgba(0,0,0,0.15)]">
+        <header className="w-full lg:py-10 lg:px-4 relative z-50">
+            <div className="max-w-[1400px] mx-auto px-4 lg:px-6 py-3 flex items-center justify-between gap-4 
+            bg-white/100 backdrop-blur-lg border border-white/60 lg:rounded-full 
+            shadow-[0_8px_30px_rgba(0,0,0,0.2)]">
 
                 {/* Logo */}
                 <div className="flex items-center">
@@ -38,7 +38,7 @@ export default function Header() {
                 </div>
 
                 {/* Desktop Navigation */}
-                <nav className="hidden lg:flex items-center gap-8 text-[#2B2E34] text-[16px] font-medium">
+                <nav className="hidden lg:flex items-center gap-7 text-[#2B2E34] text-[16px] font-medium">
                     {navLinks.map((link) => (
                         <Link key={link.name} href={link.href} className="hover:text-[#60C6B1] transition-colors">
                             {link.name}
@@ -49,7 +49,7 @@ export default function Header() {
                 {/* Desktop Buttons & Mobile Toggle */}
                 <div className="flex items-center gap-3">
                     <div className="hidden lg:flex items-center gap-3">
-                        <button className="px-10 py-3 rounded-full border border-[#60C6B1] text-[#60C6B1] hover:bg-[#60C6B1] hover:text-[#fff] cursor-pointer transition font-medium">
+                        <button className="px-8 py-3 rounded-full border border-[#60C6B1] text-[#60C6B1] hover:bg-[#60C6B1] hover:text-[#fff] cursor-pointer transition font-medium">
                             Contact us
                         </button>
 
@@ -72,7 +72,7 @@ export default function Header() {
 
             {/* Mobile Navigation Overlay */}
             <div className={`
-                fixed inset-0 z-40 transition-transform duration-300 ease-in-out top-[82px] md:top-[96px] bottom-0 lg:hidden bg-[#f9fbfb] overflow-auto
+                fixed inset-0 z-40 transition-transform duration-300 ease-in-out top-[74px] md:top-[91px] bottom-0 lg:hidden bg-[#f9fbfb] overflow-auto
                 ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
 
                 <div className="flex flex-col p-4">
