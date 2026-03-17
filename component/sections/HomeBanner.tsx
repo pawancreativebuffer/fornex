7455a84e28dca7aaaf93d50935f22b7b4707e416
pawancreativebuffer <pawansinghania@creativebuffer.com>
2026-03-17 18:15:49 +0530

    wef

--- component/sections/HomeBanner.tsx
+++ component/sections/HomeBanner.tsx
@@ -4,7 +4,7 @@ export default function HomeBanner() {
             <iframe
                 src="https://app.spline.design/file/2134e00c-4e07-4bad-984d-11f6387857f8?view=preview"
                 width="100%"
-                height="100%">
+                height="500">
             </iframe>
         </section>
     )
