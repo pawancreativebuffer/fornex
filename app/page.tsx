eed5ba03e4c0a33683ec6109c61885f9426f5d11
pawancreativebuffer <pawansinghania@creativebuffer.com>
2026-03-19 18:55:33 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,22 @@ export default function Home() {
   return (
     <>
       <section className="relative lg:h-[100vh] overflow-hidden flex justify-between flex-col pb-15 lg:pb-20 bg-[#000]">
-        <iframe
+        {/* <iframe
           className="absolute h-full scale-125 opacity-70 lg:opacity-100"
           src="https://app.spline.design/file/2134e00c-4e07-4bad-984d-11f6387857f8?view=preview"
           width="100%"
           height="100%">
-        </iframe>
+        </iframe> */}
+        <video
+          src="/images/banner2.mp4"
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="absolute top-0 left-0 w-full h-full object-cover opacity-70 lg:opacity-100"
+        >
+        </video>
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black/100 via-transparent to-black"></div>
 
         <Header />
         <HomeBanner />
