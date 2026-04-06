159e4e897258f426c44dfb9e6d217f21ec8d8db8
pawancreativebuffer <pawansinghania@creativebuffer.com>
2026-04-06 19:33:41 +0530

    w

--- component/sections/ServicesHowWeDeliver.tsx
+++ component/sections/ServicesHowWeDeliver.tsx
@@ -2,7 +2,7 @@ export default function ServiceHowWeDeliver() {
     return (
         <section className="w-full relative overflow-hidden">
             <div className="max-w-[1400px] mx-auto px-4 relative z-10">
-                wef
+
             </div>
         </section>
     )
